cdd1db9c36a19495adf0ff4574bd526a5f6c7c07
Ritter90 <Ritter90@users.noreply.github.com>
2017-07-15 00:26:38 +0100

    Created MainMenu & Credit Screens, Plus NavigationController

---
+++ Assets/Scripts/ScreenControllers/CreditsScreenController.cs
@@ -0,0 +1,25 @@
+﻿
+
+using System;
+using UnityEngine.UI;
+
+public class CreditsScreenController : BaseScreenController
+{
+    public Button backButton;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        NavigationController.Credits = this;
+
+        backButton.onClick.AddListener(backButtonClicked);
+
+        gameObject.SetActive(false);
+    }
+
+    private void backButtonClicked()
+    {
+        NavigationController.LeaveCreditsScreen();
+    }
+}
---
+++ Assets/Scripts/ScreenControllers/NavigationController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public static class NavigationController
+{
+    private static MainMenuScreenController mainMenu;
+    private static CreditsScreenController credits;
+
+    public static MainMenuScreenController MainMenu
+    {
+        set { mainMenu = value; }
+    }
+
+    public static CreditsScreenController Credits
+    {
+        set { credits = value; }
+    }
+
+    public static void PlayButtonClicked()
+    {
+
+    }
+
+    internal static void CreditsButtonClicked()
+    {
+        mainMenu.gameObject.SetActive(false);
+        credits.gameObject.SetActive(true);
+    }
+
+    internal static void LeaveCreditsScreen()
+    {
+        credits.gameObject.SetActive(false);
+        mainMenu.gameObject.SetActive(true);
+    }
+}
