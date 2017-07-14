fa8437ec4794e38a5bd6ea1f40ab6ccdbe4f350d
don6l <donalmcclean@gmail.com>
2017-07-15 00:12:40 +0100

    added spawner

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	public Transform[] spawnLocations;
+	public GameObject[] whatToSpawnPrefab;
+	public GameObject[] whatToSpawnClone;
+
+	void Start(){
+		startSpawning();
+	}
+
+
+	void startSpawning(){
+
+		for(int i = 0; i< 10; i++){
+			whatToSpawnClone[0] = Instantiate(whatToSpawnPrefab[0], spawnLocations[0].transform.position, Quaternion.Euler(0,0,0)) as GameObject;
+		}
+	}
+}
