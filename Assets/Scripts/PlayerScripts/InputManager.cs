a3f7dc380a29c791b11e2a826116523e356a3897
Ritter90 <Ritter90@users.noreply.github.com>
2017-07-15 19:15:29 +0100

    Create Input Manager

---
+++ Assets/Scripts/PlayerScripts/InputManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public static class InputManager
+{
+    public static float GetHorizontalInput(int playerNumber)
+    {
+        switch (playerNumber)
+        {
+            case 1:
+                return Input.GetAxis("Horizontal1");
+                break;
+            case 2:
+                return Input.GetAxis("Horizontal2");
+            default:
+                return 0;
+                break;
+        }
+    }
+
+    public static bool GetJumpInput(int playerNumber)
+    {
+        switch (playerNumber)
+        {
+            case 1:
+                return Input.GetButtonDown("Jump1");
+                break;
+            case 2:
+                return Input.GetButtonDown("Jump2");
+                break;
+            default:
+                return false;
+                break;
+        }
+    }
+
+    public static bool GetActionInput(int playerNumber)
+    {
+        switch (playerNumber)
+        {
+            case 1:
+                return Input.GetButtonDown("Action1");
+                break;
+            case 2:
+                return Input.GetButtonDown("Action2");
+                break;
+            default:
+                return false;
+                break;
+        }
+    }
+}
