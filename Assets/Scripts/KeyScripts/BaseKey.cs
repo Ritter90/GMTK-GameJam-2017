e955a29a8e0d7937918829abf290cf20b8a9c2b5
Ritter90 <Ritter90@users.noreply.github.com>
2017-07-15 11:50:56 +0100

    Create BaseKey Script

---
+++ Assets/Scripts/KeyScripts/BaseKey.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class BaseKey: MonoBehaviour
+{
+    protected void OnCollisionEnter2D(Collision2D coll)
+    {
+        IOpenable openable = coll.gameObject.GetComponent<IOpenable>();
+
+        if (openable != null)
+        {
+            openable.Open();
+        }
+    }
+}
